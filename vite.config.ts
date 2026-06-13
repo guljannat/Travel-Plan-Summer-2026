365482fafdcb13af7e1a80a6a23fb03bb8c56cb8
Ibrahim Khalilov <ibrahimk@vt.edu>
2026-06-13 18:46:26 -0400

    init commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    base: '/Travel-Plan-Summer-2026/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
